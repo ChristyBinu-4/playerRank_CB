eddae8c6491eef0e0a74e8aced7e79295dbe3289
ChristyBinu-4 <christy.binu91@gmail.com>
2024-07-07 20:33:18 +0530

    mistake in taking columns of dataframe

--- playerank/models/Weighter.py
+++ playerank/models/Weighter.py
@@ -76,9 +76,9 @@ class Weighter(BaseEstimator):
         # normalize the data and then eliminate the variables with zero variance
         sel = VarianceThreshold(var_threshold)
         X = sel.fit_transform(dataframe)
-        
+
         print(feature_names[0], list(sel.variances_)[0])
-        retained_features = X.columns[sel.get_support(indices=True)]
+        retained_features = dataframe.columns[sel.get_support(indices=True)]
         selected_feature_names = [feature_names[i] for i, var in enumerate(list(sel.variances_)) if var > var_threshold]
 
         if(selected_feature_names == retained_features):
